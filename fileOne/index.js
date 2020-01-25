0829ca7e8c739b284d65a0fe9480e367d565fe5b
Michael Williams <mwilliams2964@gmail.com>
2020-01-25 14:03:49 -0700

    michael's changes

--- fileOne/index.js
+++ fileOne/index.js
@@ -1,1 +1,14 @@
+<<<<<<< HEAD
 let testString = "I'm adding this to file One";
+=======
+let test = "testing"
+
+
+function console() {
+    console.log("test")
+
+}
+
+
+console()
+>>>>>>> c82285df06286c59946fa0e0528d892b7136a4a1
